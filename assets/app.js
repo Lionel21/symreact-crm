6d09bd9d49b96f61dced2864386dfb3b7844f19d
Lionel Delamare <ldelamare@entreloups.com>
2021-11-08 22:16:30 +0100

    App component created and set into App Controller

--- assets/app.js
+++ assets/app.js
@@ -5,9 +5,18 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-console.log('Hello');
+
+const App = () => {
+    return <h1>Bonjour à tous</h1>
+};
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
